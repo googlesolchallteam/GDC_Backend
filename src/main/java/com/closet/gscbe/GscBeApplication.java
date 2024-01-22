e2cbeb32edded3117c585368e01baa060852badd
JunHyuk <oj041088@gmail.com>
2024-01-22 12:12:00 +0900

    환경설정 완료

---
+++ src/main/java/com/closet/gscbe/GscBeApplication.java
@@ -0,0 +1,13 @@
+package com.closet.gscbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GscBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GscBeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/closet/gscbe/controller/RentalController.java
@@ -0,0 +1,5 @@
+package com.closet.gscbe.controller;
+
+public class RentalController {
+
+}
